34746eb166f76f1634f00c625c54b82239a0fc19
Debbie Lee <ydebbielee@gmail.com>
2018-11-13 09:43:27 -0500

    Component did mount functions to fetch photos & letters, basic layout of render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Thumbnail} from 'react-bootstrap';
+import {Thumbnail, StackGrid} from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    photos: ['https://imgur.com/a/wHjiRZg'],
+    photos: [],
     letters: []
   }
 
+  getAllPhotos = this.getAllPhotos.bind(this)
+  getAllLetters = this.getAllLetters.bind(this)
+
+  componentDidMount() {
+    this.getAllLetters();
+    this.getAllPhotos();
+  }
+  
+  getAllPhotos() {
+    axios.get('/photos')
+    .then((res) => {
+      this.setState({
+        photos: res.data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  getAllLetters() {
+    axios.get('/letters')
+    .then((res) => {
+      this.setState({
+        letters: res.data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <Thumbnail src={this.state.photos[0]}></Thumbnail>
-      </div>
-    )
+    if (!this.state.letters.length && !this.state.photos.length) {
+      return null;
+    } else {
+      return (
+        <div>
+          <StackGrid columnWidth ={300} monitorImagesLoaded={true}>
+          {this.state.photos.map((photo) => {
+            return (
+              <div key={photo.url}>
+                <Thumbnail
+                  src={photo.url}
+                  height="250"
+                  //onClick={() => function to make photo pop out bigger}
+                >
+                </Thumbnail>
+              </div>
+            )
+          })}
+          </StackGrid>
+        </div>
+      )
+    }
   }
 }
 
