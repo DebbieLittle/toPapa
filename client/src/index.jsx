6ebf526c018d458dc23300ca5b80da33dbcd9b17
Debbie Lee <ydebbielee@gmail.com>
2018-11-17 16:21:58 -0500

    Added Carousel for cards

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Thumbnail, Modal, Button} from 'react-bootstrap';
+import {Thumbnail, Modal, Carousel} from 'react-bootstrap';
 import StackGrid from 'react-stack-grid';
 import axios from 'axios';
 
@@ -8,16 +8,15 @@ class App extends React.Component {
   state = {
     photos: [],
     letters: [],
+    currentPhoto: '',
     view: false
   }
-
-  getAllPhotos = this.getAllPhotos.bind(this)
 
   componentDidMount() {
     this.getAllPhotos();
   }
 
-  getAllPhotos() {
+  getAllPhotos = () => {
     axios.get('/photos')
     .then((res) => {
       this.setState({
@@ -29,47 +28,75 @@ class App extends React.Component {
     })
   }
 
+  openModal = (photo) => {
+    this.setState({
+      currentPhoto: photo,
+      view: true
+    })
+  }
+
+  handleHide = () => {
+    this.setState({
+      view: false
+    })
+  }
+
   render() {
-    if (this.state.view === false) {
-      return (
-        <div>
-          <StackGrid columnWidth={300} monitorImagesLoaded={true}>
-          {this.state.photos.map((photo) => {
-            return (
-              <div key={photo.url}>
-                <Thumbnail
-                  src={photo.url}
-                  height="250"
-                  //onClick={() => function to make photo pop out bigger}
-                ></Thumbnail>
-              </div>
-            )
-          })}
-          </StackGrid>
-        </div>
-      )
-    } else {
-      <Modal
-          {...this.props}
-          show={this.state.show}
+    return (
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <center>
+              <img width={900} height={550} src="//i.imgur.com/qrHy0NK.png" />
+            </center>
+          </Carousel.Item>
+          <Carousel.Item>
+            <center>
+              <img width={900} height={550} src="//i.imgur.com/vKrYKxw.png" />
+            </center>
+          </Carousel.Item>
+          <Carousel.Item>
+            <center>
+              <img width={900} height={550} src="//i.imgur.com/buheJ7G.png" />
+            </center>
+          </Carousel.Item>
+          <Carousel.Item>
+            <center>
+              <img width={900} height={550} src="//i.imgur.com/c4aJ35z.png" />
+            </center>
+          </Carousel.Item>
+        </Carousel>
+
+        <StackGrid columnWidth={300} monitorImagesLoaded={true}>
+        {this.state.photos.map((photo) => {
+          return (
+            <div key={photo.url}>
+              <Thumbnail
+                src={photo.url}
+                height="250"
+                onClick={() => this.openModal(photo.url)}
+              ></Thumbnail>
+            </div>
+          )
+        })}
+        </StackGrid>
+
+        <Modal
+          show={this.state.view}
           onHide={this.handleHide}
-          dialogClassName="custom-modal"
+          bsSize="large"
         >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-lg">
-              Modal heading
-            </Modal.Title>
-          </Modal.Header>
           <Modal.Body>
-            <h4>Wrapped Text</h4>
-            <p>
-            </p>
+            <center>
+              <img
+                style={{ maxWidth: '870px', maxHeight: '750px' }}
+                src={this.state.currentPhoto}
+              />
+            </center>
           </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleHide}>Close</Button>
-          </Modal.Footer>
         </Modal>
-    }
+      </div>
+    )
   }
 }
 
